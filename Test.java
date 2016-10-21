23a69251d68bc469db2f456d5b055d973c9e8343
Wind_Protocol <3dsarcard@gmail.com>
2016-10-21 10:48:55 -0500

    updated test for cleverbot example

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import apis.CleverbotApi;
 import prottoapi.ClientSession;
 import prottoapi.ResponseObject;
 
@@ -8,24 +9,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		try {
-			String osuKey = "apikey";
-			String osuLink = "https://osu.ppy.sh/api/get_user";
-			String userQuery = "randomuser";
-			
-			ClientSession session = new ClientSession(1);
-			
-			JSONObject params = new JSONObject();
-			params.put("u", userQuery);
-			params.put("k", osuKey);
-			
-			ResponseObject resp = session.get(osuLink, params);
-			if (resp.onReady()) 
-			{
-				JSONArray info = new JSONArray(resp.text());
-				System.out.println("Array: \n" + info.toString());
-			}
-			
-			session.close();
+			CleverbotApi cleverbot = new CleverbotApi();
+			String response = cleverbot.ask("Hey how are you?");
+			System.out.println("Response: " + response);
 		}
 		catch (Exception e) 
 		{
